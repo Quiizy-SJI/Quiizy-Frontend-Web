f8215a6d844bd0ada79cca927385f5e4b9af8b51
Alex Nelson <alex.nelson.bryan@gmail.com>
2025-12-26 10:10:54 +0100

    refactor(services): export updates

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -2,6 +2,8 @@ export * from './sse.service';
 export * from './event-types';
 export * from './theme.service';
 
+export * from './dean-api.service';
+
 export * from '../core/auth/auth.service';
 export * from '../core/auth/auth-store.service';
 export * from '../core/http/api-client.service';
