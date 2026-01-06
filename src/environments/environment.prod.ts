f633bb1c5711c37a9bde45c4c1984217a54c87d6
NinjaShadowBoy <alex.nelson.bryan@gmail.com>
2026-01-06 16:57:23 +0100

    feat: Add the backend production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // TODO: set your production API base URL
-  apiBaseUrl: 'https://api.quizzy.app',
+  apiBaseUrl: 'https://quizzy-backend-api.onrender.com',
 };
