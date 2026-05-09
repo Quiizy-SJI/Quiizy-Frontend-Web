fdc161d0c8a0ff5fbf2cc115f94fb6cd45a09ba2
Abena Alex <alex.nelson.bryan@gmail.com>
2026-05-09 16:38:19 +0100

    fix: make the quizz selection card andgrid scollable on  Change api url to cloudfare tunnel.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // API endpoint with /quizzy/api prefix
-  apiBaseUrl: '/quizzy/api',
+  // API endpoint with /quizzy/api prefix (Cloudflare Tunnel)
+  apiBaseUrl: 'https://rod-gospel-interactive-miller.trycloudflare.com/quizzy/api',
 };
