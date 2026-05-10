6933bb82ab788f69b39fcb81fff68abee6fea92b
Abena Alex <alex.nelson.bryan@gmail.com>
2026-05-10 10:19:11 +0100

    chore: change api url to my cloudfare tunnel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // API endpoint with /quizzy/api prefix (Cloudflare Tunnel)
-  apiBaseUrl: 'https://rod-gospel-interactive-miller.trycloudflare.com/quizzy/api',
+  apiBaseUrl: 'https://vps.alexabena.me/quizzy/api',
 };
